f17e232f79c2d7265b66ea84c4f1707749119c69
vcasellesb <vcasellesb@uoc.edu>
2024-05-12 10:31:32 +0200

    Changes from yesterday

--- R5/test/basic-stuff-I-should-already-know.R
+++ R5/test/basic-stuff-I-should-already-know.R
@@ -21,3 +21,54 @@ integrate(f_normal_standard, -Inf, 1.96) # 0.975
 integrate(f_normal_standard, -Inf, -1.96) # should be 0.025 (it is)
 integrate(f_normal_standard, -Inf, 1.645) # 0.95
 integrate(f_normal_standard, -Inf, Inf) # 1
+
+# Let's try out another function
+f <- function(x){
+  cond <- (x > 2) | (x < 0)
+  if (cond) return(0)
+  else return(1/2)
+}
+
+int_f <- function(x){
+  x / 2
+}
+
+################################################################
+# Studying from: https://www.stat.uchicago.edu/~stigler/Stat244/ch3withfigs.pdf
+# Let's define our function:
+f_x_y <- function(x,y) {
+  y * (1/2 - x) + x
+}
+fy <- function(y){
+  1/2 * y
+}
+
+integrate(fy, lower=0, upper=2)
+x <- seq(0, 1, length=100)
+y <- seq(0,2,length=100)
+z <- outer(x,y,f_x_y)
+persp(x, y, z,
+      theta=30, phi=20,
+      r=50, d=0.1, expand=0.5,
+      ltheta=90, lphi=180,
+      shade=0.75,
+      ticktype="detailed",
+      cex.axis=0.7,
+      zlab="density"
+)
+
+f_x_cond_y <- function(x, y){
+  y * (1 - 2*x) + 2*x
+}
+x <- seq(0, 1, length=100)
+y <- seq(0,2,length=100)
+z <- outer(x,y,f_x_cond_y)
+persp(x, y, z,
+      theta=30, phi=20,
+      r=50, d=0.1, expand=0.5,
+      ltheta=90, lphi=180,
+      shade=0.75,
+      ticktype="detailed",
+      cex.axis=0.7,
+      zlab="density"
+)
