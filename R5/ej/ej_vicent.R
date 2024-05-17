6a9897bf35290d862cf2af6dd950a7fc4bc76aa9
vcasellesb <vcasellesb@uoc.edu>
2024-05-17 11:56:05 +0200

    progress on exercises

--- R5/ej/ej_vicent.R
+++ R5/ej/ej_vicent.R
@@ -15,3 +15,41 @@ colSums(dt) # marginal distributions of cols
 
 # conditional probabilities given columns
 t(t(dt) / colSums(dt))
+
+## Exercici 5
+
+# generem el que es demana
+n <- 10000
+x1 <- rnorm(n)
+x2 <- rnorm(n)
+X <- cbind(x1, x2)
+
+# c)
+y1_ <- function(x1, mu1, sigma1){
+  mu1 + sigma1 * x1
+}
+
+y2_ <- function(x1, x2, mu2, sigma2, rho){
+  mu2 + sigma2 * (rho*x1 + sqrt(1-rho**2)*x2)
+}
+
+# parameter definition
+mu <- c(2, 1)
+sigma1 <- 1
+sigma2 <- 1.5
+rho <- 0.6
+Y <- cbind(y1_(X[,1], mu[1], sigma1), y2_(X[,1], X[,2], mu[2], sigma2,rho))
+
+colMeans(Y)
+
+
+N2 <- X
+transform <- function(x,mu1,mu2,s1,s2,rho){
+   y1 <- mu1 + s1*x[1]
+ y2 <- mu2 + s2*rho*x[1] + s2*sqrt(1-rho^2)*x[2]
+ c(y1,y2)
+}
+N2Y <- t(apply(N2,1,transform,mu1=2,mu2=1,s1=1,s2=1.5,rho=0.6))  
+colnames(N2Y) <- c("y1","y2")
+plot(N2Y, pch=19, col=rgb(1,0,0,alpha = 0.3))
+max(abs(Y - N2Y))
