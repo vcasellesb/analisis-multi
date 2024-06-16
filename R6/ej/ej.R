a493c62ba59ff8ed54a907b673ceff21221fa36c
vcasellesb <vcasellesb@uoc.edu>
2024-06-16 13:32:33 +0200

    R6 code

--- R6/ej/ej.R
+++ R6/ej/ej.R
@@ -29,8 +29,6 @@ for (i in 1:(nr-1)){
 }
 d
 
-
-
 R_euclid <- function(x, nc, nr, i1, i2){
 
   res <- 0
@@ -68,3 +66,36 @@ sum(evalues[1:2]) / sum(evalues[1:5])
 
 plot(X,type="n", xlab="PC1",ylab="PC2",xlim=c(-30,50))
 text(X[,1],X[,2],labels=rownames(d),cex=0.8)
+
+## trying stuff about Batachartya whatever distances
+pg <- matrix(c(0.21,0.06,0.06,0.67,
+               0.25,0.04,0.14,0.57,
+               0.22,0.06,0.08,0.64,
+               0.19,0.04,0.02,0.75,
+               0.18,0.00,0.15,0.67,
+               0.23,0.00,0.28,0.49,
+               0.30,0.00,0.06,0.64,
+               0.10,0.06,0.13,0.71,
+               0.27,0.04,0.06,0.63,
+               0.21,0.05,0.20,0.54), ncol=4, byrow=T)
+colnames(pg) <- c("gr.A","gr.B","gr.AB","gr.0")
+rownames(pg) <- c("francesa","checa","germánica","vasca","china",
+                  "ainu","esquimal","negra_USA","española","egipcia")
+
+# B distance calculation
+# first we need to calculate frequencies per row.
+freq <- t(t(milk) / rowSums(milk))
+# Now we calculate sqrt of this
+Q <- sqrt(freq)
+D_BC <- Q %*% t(Q)
+D_BC <- ifelse(D_BC>1, 1, D_BC)
+D_BC <- acos(D_BC)
+
+B <- B_from_D(sqrt(D_BC))
+evecs2 <- eigen(B)$vectors[, 1:2]
+evalues2 <- eigen(B)$values[1:2]
+X <- evecs2 %*% diag(sqrt(evalues2))
+
+cmdscale(sqrt(D_BC), eig = T)
+
+
